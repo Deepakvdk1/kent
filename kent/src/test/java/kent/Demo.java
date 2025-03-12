f8414fd60e8c79c29c1de06d6b0846e6aa1df4ef
deepa <deepa@SOUL_MACHINE>
2025-03-12 11:30:55 +0530

    added weight stmt

--- kent/src/test/java/kent/Demo.java
+++ kent/src/test/java/kent/Demo.java
@@ -1,19 +1,24 @@
 package kent;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Demo {
-	
+	WebDriver driver;
 	@Test
 	public void mavencheck() {
 		Reporter.log("mavencheck",true);
-		WebDriver driver= new ChromeDriver();
+		driver= new ChromeDriver();
 		driver.get("https://bbb.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		String title=driver.getTitle();
 		Reporter.log(title,true);
+		
 	}
 
 }
